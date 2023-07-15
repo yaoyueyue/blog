d0e5fd952a26a03556bb5a9ba5b97854ad39284d
Yue Yao <amanday0021@gmail.com>
2023-07-15 13:31:13 -0400

    fixed errors in app.js, package.json,root.js

--- blog-frontend/src/Root.js
+++ blog-frontend/src/Root.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from 'components/App';
+import App from './components/App';
 import { Provider } from 'react-redux';
-import configure from 'store/configure';
+import configure from './store/configure';
 import { Route } from 'react-router-dom';
 
 const store = configure();
--- blog-frontend/src/components/App.js
+++ blog-frontend/src/components/App.js
@@ -1,38 +1,38 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { PostPage, PostListPage, EditorPage, NotFoundPage } from 'pages';
-import LoginContainer from 'containers/LoginContainer'
+import { Switch, Route as RouterSwitch } from 'react-router-dom';
+import { PostPage, PostListPage, EditorPage, NotFoundPage } from '../pages';
+import LoginContainer from '../containers/LoginContainer'
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
-import * as authActions from "store/modules/auth";
-import OAuth2RedirectHandler from 'components/Login/oauth2/OAuth2RedirectHandler';
+import * as authActions from "../store/modules/auth";
+import OAuth2RedirectHandler from './Login/oauth2/OAuth2RedirectHandler';
 
 class App extends Component {
 
   componentDidMount() {
-    const { AuthActions } = this.props;    
+    const { AuthActions } = this.props;
     AuthActions.getUser();
   }
 
   render() {
     return (
       <div>
-        <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}/>      
-        <Route path="/login" component={LoginContainer} />       
-        <Switch>
+        <Route path="/oauth2/redirect" component={OAuth2RedirectHandler} />
+        <Route path="/login" component={LoginContainer} />
+        <RouterSwitch>
           <Route path="/pages/:page" component={PostListPage} />
           <Route path="/posts/:id" component={PostPage} />
-          <Route path="/editor/:id?" component={EditorPage} />          
+          <Route path="/editor/:id?" component={EditorPage} />
           <Route path="/" component={PostListPage} />
           <Route component={NotFoundPage} />
-        </Switch>       
+        </RouterSwitch>
       </div>
     );
   }
 };
 
 export default connect(
-  state => ({   
+  state => ({
   }),
   dispatch => ({
     AuthActions: bindActionCreators(authActions, dispatch)
